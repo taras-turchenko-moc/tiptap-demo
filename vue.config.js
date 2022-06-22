15938e77a65e2d2c74a7ae4007480db5cc09aaa7
Taras <taras.turchenko@masterofcode.com>
2022-06-22 13:04:11 +0300

    fix assets

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const path = require('path');
+
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
+  transpileDependencies: ['vuetify'],
+  publicPath: '/tiptap-demo/',
+  outputDir: path.resolve(__dirname, './docs'),
+  productionSourceMap: false
 })
